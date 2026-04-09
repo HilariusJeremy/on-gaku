e58f006d010e17de87dba38cfa8dab5ac0e9bfb2
hilariusjeremy <hilariusjeremy@gmail.com>
2026-04-09 15:43:04 +0800

    Add backend for bounding box extraction, static file serving, and CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
-from .omr import parse_musicxml
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from .omr import parse_musicxml, extract_measure_bboxes
 import os, argparse
 from oemer.ete import extract
+from oemer import layers
+
 
 app = FastAPI()
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -25,8 +42,22 @@ async def annotate(file: UploadFile = File(...)):
                                 save_cache=True,
                                 without_deskew=False)
     extract(args)
+
     basename = os.path.splitext(fname)[0]
     xmlpath = os.path.join(UPLOAD_DIR, f"{basename}.musicxml")
     result = parse_musicxml(xmlpath)
+
+    img = layers.get_layer('original_image')
+    staffs = layers.get_layer('staffs')
+    barlines = layers.get_layer('barlines').tolist()
+    num_groups = staffs.tolist()[0][-1].group + 1
+
+    bbox_coords = extract_measure_bboxes(num_groups, staffs, barlines, img.shape[1])
+    
+    # attach bboxes to measures
+    for measure, bbox in zip(result['measures'], bbox_coords):
+        x1, y1, x2, y2 = bbox
+        measure['bbox'] = {'x1': int(x1), 'y1': int(y1), 'x2': int(x2), 'y2': int(y2)}
+
     result['img_url'] = f"/uploads/{fname}"
     return result
