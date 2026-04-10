8c199edb42336049e50687fc41971ecb3bbad7e4
hilariusjeremy <hilariusjeremy@gmail.com>
2026-04-10 15:13:27 +0800

    Add sidebar measure-wise note visualization

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,23 @@
-const Sidebar = () => {
-    return <h1>
-        Sidebar
-    </h1>
+import type { Measure } from "../App"
+
+const Sidebar = ({selectedMeasure}: {selectedMeasure: Measure}) => {
+    console.log(selectedMeasure)
+    if (!selectedMeasure) return
+    return (
+    <div>
+        <h1>
+            Sidebar
+        </h1>
+        <p>Treble</p>    
+        <ul>
+                {selectedMeasure.treble.map((note, index) => <li key={index}>{note}</li>)}
+        </ul>
+        <p> Bass</p>
+        <ul>
+                {selectedMeasure.bass.map((note, index) => <li key={index}>{note}</li>)}
+        </ul>
+    </div>
+    )
 }
 
 export default Sidebar
