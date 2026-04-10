58d1f780bcbd555bc28a9cb89930798c2ea71d7a
hilariusjeremy <hilariusjeremy@gmail.com>
2026-04-10 15:12:41 +0800

    Add bounding box highlighting

--- frontend/src/components/ScoreViewer.tsx
+++ frontend/src/components/ScoreViewer.tsx
@@ -1,8 +1,46 @@
-const ScoreViewer = () => {
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import type { AnnotationData, Measure } from '../App';
+import './ScoreViewer.css';
+
+
+const ScoreViewer = ({result, onSelectedMeasure}: {result: AnnotationData|null, onSelectedMeasure: (measure: Measure) => void}) => {
+    const [scale, setScale] = useState(1)
+    const imgRef = useRef<HTMLImageElement>(null)
+
+    useEffect(() => {
+    if (!imgRef.current) return
+    if (imgRef.current.complete) {
+        handleLoad()
+    }
+}, [])
+
+    if (!result) return null
+
+
+    const measures = result['measures']
+    const measure_bbox = measures.map(measure => <div 
+            className='bbox'
+            style={{
+                left: measure.bbox.x1*scale, top: measure.bbox.y1*scale, height: (measure.bbox.y2 - measure.bbox.y1)*scale,
+                width: (measure.bbox.x2 - measure.bbox.x1)*scale
+            }}
+            key={measure.number}
+            onClick={() => onSelectedMeasure(measure)}
+            ></div>)
+
+    const handleLoad = () => {
+        if (!imgRef.current) return
+        setScale(imgRef.current.getBoundingClientRect().width / imgRef.current.naturalWidth)
+    }
+
     return (
         <div>
             <h1>ScoreViewer</h1>
-            <img src='/uploads/sabbath_short.png'/>
+            <div className='score-viewer'>
+                <img className='sheet-img' onLoad={handleLoad} src={result['img_url']} ref={imgRef}/>
+                {measure_bbox}
+            </div>
         </div>
     )
 }
