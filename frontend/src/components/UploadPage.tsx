77fe7bd0af9b7a1b91392f95a4acabed50847f1a
hilariusjeremy <hilariusjeremy@gmail.com>
2026-04-11 00:44:56 +0800

    Add frontend for loading screen

--- frontend/src/components/UploadPage.tsx
+++ frontend/src/components/UploadPage.tsx
@@ -1,18 +1,29 @@
+import { useRef, useState } from 'react';
 import type {AnnotationData} from '../App'
-
+import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
 const UploadPage = ({onResult}: {onResult: (data: AnnotationData) => void}) => {
+    const [loading, setLoading] = useState(false)
+    const controllerRef = useRef<AbortController | null>(null)
+    const onAbort = () => {
+        if(!controllerRef.current) return
+        controllerRef.current.abort()
+        setLoading(false)
+    }
+    
     const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
         const formData = new FormData();
         if (!e.target.files) return
         const file = e.target.files[0]
         if (!file) return
+        setLoading(true)
         formData.append('file', file)
-        fetch('/annotate', {method: 'POST', body: formData})
+        controllerRef.current = new AbortController()
+        fetch('/annotate', {method: 'POST', body: formData, signal: controllerRef.current?.signal})
             .then(res => res.json())
             .then(
                 data => {
                     onResult(data)
-                    console.log(data)
+                    setLoading(false)
                 }
                 
                     );
@@ -22,6 +33,11 @@ const UploadPage = ({onResult}: {onResult: (data: AnnotationData) => void}) => {
     <div>
         <h1>Upload Page</h1>
         <input type="file" accept="image/png, image/jpeg" onChange={handleSubmit}/> 
+        {loading ? 
+            <div>
+                <button onClick={onAbort}>Cancel</button>
+                <LoadingSpinner/>
+            </div>: <div/>}
     </div>)
 
 }
